e16221013a9143a6be7fa5e3112df66052034d30
MavericCod <921chavanprem@gmail.com>
2024-09-04 23:24:36 +0530

    added random image as get request

--- index.js
+++ index.js
@@ -31,6 +31,32 @@ app.get("/api/jokes/random", async (req, res) => {
   }
 });
 
+app.get("/api/joke-image/random", async (req, res) => {
+  try {
+    const imageResponse = await axios({
+      method: "get",
+      url: `https://api.unsplash.com/photos/random?query=nature&client_id=${process.env.UNSPLASH_API_KEY}`,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const imageUrl = {
+      id: imageResponse.data.id,
+      url: imageResponse.data.urls.regular,
+    };
+
+    res.json(imageUrl);
+  } catch (err) {
+    console.error("Error details:", err.response ? err.response.data : err.message);
+    res.status(500).json({
+      error: "An error occurred while fetching the image",
+      details: err.response ? err.response.data : err.message,
+    });
+  }
+});
+
+
 app.listen(port, () => {
   console.log("Server started at " + port);
 });
